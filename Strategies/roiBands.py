756f67d4a394f96973d05eb44365b816842cfb89
natebelete1 <natebelete@gmail.com>
2022-11-17 04:24:58 -0800

    update to script

--- Strategies/roiBands.py
+++ Strategies/roiBands.py
@@ -11,7 +11,7 @@ class roiBands(bt.Strategy):
     # list of parameters which are configurable for the strategy
     params = dict(
         
-        ratio = 0.20
+        ratio = 0.50
     )
 
 
@@ -62,6 +62,10 @@ class roiBands(bt.Strategy):
         # total holds
         self.holdings = 0
 
+        # counts the number of triggers
+        self.buy_triggers = 0
+        self.sell_triggers = 0
+
 
 
 
@@ -107,6 +111,8 @@ class roiBands(bt.Strategy):
         # Write down: no pending order
         self.order = None
 
+        
+
 
     def next(self):
         # Check if an order is pending ... if yes, we cannot send a 2nd one
@@ -122,72 +128,79 @@ class roiBands(bt.Strategy):
 
 
 
-        if len(self.close_) > 15 :
-            self.close_p15_max.append(np.max(self.close_[-15:]))
-            self.close_p15_min.append(np.min(self.close_[-15:]))
+        if len(self.close_) > 21 :
+            self.close_p15_max.append(np.max(self.close_[-21:]))
+            self.close_p15_min.append(np.min(self.close_[-21:]))
 
             self.roi_from_high.append(self.close_[-1] / self.close_p15_max[-1] - 1)
             self.roi_from_low.append(self.close_[-1] / self.close_p15_min[-1] - 1 )
 
             self.roi_from_high_10th_pct.append(np.quantile(self.roi_from_high,0.05))
-            self.roi_from_low_90th_pct.append(np.quantile(self.roi_from_low,0.80))
-
-            # print(self.date_[-1])
-            # print(self.close_[-1])
-            # print(self.close_p15_max[-1])
-            # print(self.roi_from_low_90th_pct[-1])
-
-            if (self.roi_from_high[-1] <= self.roi_from_high_10th_pct[-1]) & (self.roi_from_low[-1] < self.roi_from_low_90th_pct[-1]):
-                # BUY, BUY, BUY!!! (with default parameters)
-                self.log('BUY CREATED, %.2f' % self.dataclose[0])
+            self.roi_from_low_90th_pct.append(np.quantile(self.roi_from_low,0.95))
 
 
+            if (self.roi_from_high[-1] <= self.roi_from_high_10th_pct[-1]
+            ) & (self.roi_from_low[-1] < self.roi_from_low_90th_pct[-1]):
+                
+                while self.buy_triggers > 10:
+                    # BUY, BUY, BUY!!! (with default parameters)
+                    self.log('BUY CREATED, %.2f' % self.dataclose[0])
 
-                # the currenr account value
-                account_value = self.broker.get_cash() 
+                    # the currenr account value
+                    account_value = self.broker.get_cash() 
 
-                # the number of shares I can buy
-                total_shares = round(account_value * self.p.ratio / self.dataclose[0])
+                    # the number of shares I can buy
+                    total_shares = round(account_value * self.p.ratio / self.dataclose[0])
 
-                # Keep track of the created order to avoid a 2nd order
-                self.order = self.buy(size=total_shares)
+                    # Keep track of the created order to avoid a 2nd order
+                    self.order = self.buy(size=total_shares)
 
-                if total_shares >= 1:
-                    self.holdings += 1
-                    self.total_shares_holding.append(total_shares)
+                    if total_shares >= 1:
+                        self.holdings += 1
+                        self.total_shares_holding.append(total_shares)
 
-                            # print order info
-                self.orderInfo(total_shares)
+                                # print order info
+                    self.orderInfo(total_shares)
 
-                            # keep track of account value
-                self.account_Value.append(cerebro.broker.getvalue())
+                                # keep track of account value
+                    self.account_Value.append(cerebro.broker.getvalue())
+
+                    self.buy_triggers = 0
+
+                self.buy_triggers += 1
 
         
             if  (self.roi_from_low[-1] >= self.roi_from_low_90th_pct[-1]) & (
                 self.roi_from_high[-1] > self.roi_from_high_10th_pct[-1]):
                 #& (len(self.total_shares_holding)>0):
 
-        # if 
-                self.log('SELL CREATED, %.2f' % self.dataclose[0])
-
-                    # place sell order
-                # self.order = self.sell(size = self.total_shares_holding[0]) 
-                # sell all shares 
                 tot_shares = sum(self.total_shares_holding)
-                self.order = self.sell(size = tot_shares) 
+
+                if (tot_shares > 0 ):
+                    while self.sell_triggers >= 5:
+                        self.log('SELL CREATED, %.2f' % self.dataclose[0])
+
+                            # place sell order
+                        # self.order = self.sell(size = self.total_shares_holding[0]) 
+                        # sell all shares 
+                        
+                        self.order = self.sell(size = tot_shares) 
 
 
-                    
-                self.orderInfo(tot_shares)
+                            
+                        self.orderInfo(tot_shares)
 
-                    # reduce holding position
-                self.holdings -= 1
+                            # reduce holding position
+                        self.holdings -= 1
 
-                    # update remaining positions
-                self.total_shares_holding = [0]
+                            # update remaining positions
+                        self.total_shares_holding = [0]
 
-        data = {'Date': self.date_[15:],
-                'Close': self.close_[15:],
+                        self.sell_triggers = 0
+                    self.sell_triggers += 1
+
+        data = {'Date': self.date_[21:],
+                'Close': self.close_[21:],
                 'roi_from_high': self.roi_from_high,
                 'roi_from_low': self.roi_from_low,
                 'roi_from_high_10th_pct': self.roi_from_high_10th_pct,
--- trade.py
+++ trade.py
@@ -5,7 +5,7 @@ import numpy as np
 from Strategies.roiBands import roiBands
 
 # get data
-df = yf.download('SBUX', '2022-01-01', '2022-12-31', interval = '1d')
+df = yf.download('vxx', '2022-01-10', '2022-11-16', interval = '1h')
 
 # establish an instance
 cerebro = bt.Cerebro()
@@ -20,7 +20,7 @@ cerebro.adddata(feed)
 cerebro.addstrategy(roiBands)
 
 # Set initial account value
-cerebro.broker.setcash(10000)
+cerebro.broker.setcash(100000)
 
 
 # Print out the starting conditions
@@ -32,7 +32,7 @@ cerebro.run()
 print('Final Portfolio Value: %.2f' % cerebro.broker.getvalue())
 
 # Return if buy and hold 
-hold = df['Close'][-1]/df['Close'][0]*10000
+hold = df['Close'][-1]/df['Close'][0]*100000
 print("Return if you just held position {}".format(hold))
 
 cerebro.plot()
